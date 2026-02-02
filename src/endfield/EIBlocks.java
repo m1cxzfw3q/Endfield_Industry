f0ee2df30e35c9647f2fe6bf5a029410d6c31442
m1cxzfw3q <mcxzfwq@outlook.com>
2026-02-02 08:59:27 +0800

    test

--- src/endfield/EIBlocks.java
+++ src/endfield/EIBlocks.java
@@ -1,22 +1,16 @@
 package endfield;
 
-import arc.Core;
 import endfield.special.EIConveyorBlock;
 import endfield.special.ProtocolCoreBlock;
 import endfield.special.ProtocolStorageBoxBlock;
-import mindustry.gen.Building;
+import endfield.special.ThermalEnergyPoolBlock;
 import mindustry.graphics.Drawf;
-import mindustry.graphics.Pal;
 import mindustry.type.Category;
-import mindustry.type.Item;
 import mindustry.type.ItemStack;
 import mindustry.world.Block;
-import mindustry.world.blocks.power.ConsumeGenerator;
-import mindustry.world.blocks.power.LongPowerNode;
+import mindustry.world.blocks.power.PowerNode;
 import mindustry.world.consumers.ConsumeItemList;
 import mindustry.world.meta.Stat;
-
-import static mindustry.Vars.world;
 
 public class EIBlocks {//武陵的内容我想等武陵更完再更
     public static Block
@@ -32,16 +26,14 @@ public class EIBlocks {//武陵的内容我想等武陵更完再更
         protocolCore = new ProtocolCoreBlock("protocol-core");
         protocolStorageBox = new ProtocolStorageBoxBlock("protocol-storage-box");
 
-        repeater = new LongPowerNode("repeater") {
+        repeater = new PowerNode("repeater") {
             {
                 size = 3;
                 maxNodes = 1000;
                 laserRange = 80;
                 autolink = false;
                 requirements(Category.power, ItemStack.with(EIItems.crystalShell, 20));
-                glowColor = laserColor2 = Pal.powerLight;
-                laser = Core.atlas.find("power-beam");
-                laserEnd = Core.atlas.find("power-beam-end");
+                drawRange = false;
             }
             @Override
             public void setBars() {
@@ -63,7 +55,7 @@ public class EIBlocks {//武陵的内容我想等武陵更完再更
 
         conveyor = new EIConveyorBlock("conveyor");
 
-        thermalEnergyPool = new ConsumeGenerator("thermal-energy-pool") {{
+        thermalEnergyPool = new ThermalEnergyPoolBlock("thermal-energy-pool") {{
             size = 2;
             itemDuration = 10f * 60;
             itemDurationMultipliers.put(EIItems.low_capacityValleyBattery, 4);
@@ -82,34 +74,7 @@ public class EIBlocks {//武陵的内容我想等武陵更完再更
             }});
 
             itemCapacity = 50;
-            buildType = ThermalEnergyPoolBuild::create;
             requirements(Category.power, ItemStack.with(EIItems.crystalShell, 10, EIItems.amethystComponent, 10));
-        }
-            public class ThermalEnergyPoolBuild extends ConsumeGeneratorBuild {//写世处写的   能跑就行
-                @Override
-                public boolean acceptItem(Building source, Item item) {
-                    Building b1 = null, b2 = null;
-                    switch (rotation) {
-                        case 0 -> {
-                            b1 = world.tile((int) x / 8 + 2 , (int) y / 8).build;
-                            b2 = world.tile((int) x / 8 + 2 , (int) y / 8 + 1).build;
-                        }
-                        case 1 -> {
-                            b1 = world.tile((int) x / 8 , (int) y / 8 + 2).build;
-                            b2 = world.tile((int) x / 8 + 1 , (int) y / 8 + 2).build;
-                        }
-                        case 2 -> {
-                            b1 = world.tile((int) x / 8 - 1 , (int) y / 8).build;
-                            b2 = world.tile((int) x / 8 - 1 , (int) y / 8 + 1).build;
-                        }
-                        case 3 -> {
-                            b1 = world.tile((int) x / 8 , (int) y / 8 - 1).build;
-                            b2 = world.tile((int) x / 8 + 1 , (int) y / 8 - 1).build;
-                        }
-                    }
-                    return super.acceptItem(source, item) && (b1 == source || b2 == source);
-                }
-            }
-        };
+        }};
     }
 }
---
+++ src/endfield/special/ThermalEnergyPoolBlock.java
@@ -0,0 +1,38 @@
+package endfield.special;
+
+import mindustry.gen.Building;
+import mindustry.type.Item;
+import mindustry.world.blocks.power.ConsumeGenerator;
+
+import static mindustry.Vars.world;
+
+public class ThermalEnergyPoolBlock extends ConsumeGenerator {
+    public ThermalEnergyPoolBlock(String name) {
+        super(name);
+    }
+    public class ThermalEnergyPoolBuild extends ConsumeGeneratorBuild {//写世处写的   能跑就行
+        @Override
+        public boolean acceptItem(Building source, Item item) {
+            Building b1 = null, b2 = null;
+            switch (rotation) {
+                case 0 -> {
+                    b1 = world.tile((int) x / 8 + 2 , (int) y / 8).build;
+                    b2 = world.tile((int) x / 8 + 2 , (int) y / 8 + 1).build;
+                }
+                case 1 -> {
+                    b1 = world.tile((int) x / 8 , (int) y / 8 + 2).build;
+                    b2 = world.tile((int) x / 8 + 1 , (int) y / 8 + 2).build;
+                }
+                case 2 -> {
+                    b1 = world.tile((int) x / 8 - 1 , (int) y / 8).build;
+                    b2 = world.tile((int) x / 8 - 1 , (int) y / 8 + 1).build;
+                }
+                case 3 -> {
+                    b1 = world.tile((int) x / 8 , (int) y / 8 - 1).build;
+                    b2 = world.tile((int) x / 8 + 1 , (int) y / 8 - 1).build;
+                }
+            }
+            return super.acceptItem(source, item) && (b1 == source || b2 == source);
+        }
+    }
+}

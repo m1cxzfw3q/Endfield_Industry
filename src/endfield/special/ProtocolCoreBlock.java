148bd0f0ff7b9b178ac290e92ab87f72385ce3f6
m1cxzfw3q <mcxzfwq@outlook.com>
2026-02-03 17:44:41 +0800

    test

--- src/endfield/special/ProtocolCoreBlock.java
+++ src/endfield/special/ProtocolCoreBlock.java
@@ -1,62 +1,252 @@
 package endfield.special;
 
 import arc.Core;
+import arc.func.Boolf;
+import arc.func.Cons;
+import arc.graphics.Color;
+import arc.graphics.g2d.Draw;
+import arc.graphics.g2d.TextureRegion;
 import arc.math.Mathf;
+import arc.math.geom.Intersector;
+import arc.math.geom.Point2;
 import arc.struct.EnumSet;
+import arc.struct.IntSeq;
+import arc.struct.ObjectSet;
 import arc.struct.Seq;
+import arc.util.Nullable;
 import arc.util.Strings;
+import arc.util.Structs;
+import arc.util.Tmp;
 import arc.util.io.Reads;
 import arc.util.io.Writes;
+import mindustry.core.Renderer;
+import mindustry.game.Team;
 import mindustry.gen.Building;
+import mindustry.graphics.Drawf;
+import mindustry.graphics.Layer;
 import mindustry.graphics.Pal;
 import mindustry.type.Category;
 import mindustry.type.ItemStack;
 import mindustry.ui.Bar;
+import mindustry.world.Block;
+import mindustry.world.Edges;
+import mindustry.world.Tile;
+import mindustry.world.blocks.power.PowerGraph;
+import mindustry.world.blocks.power.PowerNode;
 import mindustry.world.blocks.storage.CoreBlock;
 import mindustry.world.meta.BlockFlag;
 import mindustry.world.meta.BlockStatus;
 import mindustry.world.meta.BuildVisibility;
+import mindustry.world.modules.PowerModule;
+
+import static mindustry.Vars.tilesize;
+import static mindustry.Vars.world;
 
 public class ProtocolCoreBlock extends CoreBlock {
     public boolean isSub = false;
     public float powerProduction = 200 / 60f;
+    public int powerStorage = 10000;
+
+    public TextureRegion laser = Core.atlas.find("laser"), laserEnd = Core.atlas.find("laser-end");
+
+    public static float laserRange = 80;
+    public int maxNodes = 1000;
+    public boolean autolink = true, drawRange = false, sameBlockConnection = false;
+
+
+    protected final static ObjectSet<PowerGraph> graphs = new ObjectSet<>();
 
     public ProtocolCoreBlock(String name) {
         super(name);
         size = 9;
         itemCapacity = 8000;
         requirements(Category.effect, BuildVisibility.hidden, ItemStack.with());
-        consumePowerBuffered(10000);
         outputsPower = true;
         consumesPower = true;
         hasPower = true;
         flags = EnumSet.of(BlockFlag.generator, BlockFlag.core, BlockFlag.battery);
+
+        config(Integer.class, (entity, value) -> {
+            PowerModule power = entity.power;
+            Building other = world.build(value);
+            boolean contains = power.links.contains(value), valid = other != null && other.power != null;
+
+            if(contains){
+                //unlink
+                power.links.removeValue(value);
+                if(valid) other.power.links.removeValue(entity.pos());
+
+                PowerGraph newgraph = new PowerGraph();
+
+                //reflow from this point, covering all tiles on this side
+                newgraph.reflow(entity);
+
+                if(valid && other.power.graph != newgraph){
+                    //create new graph for other end
+                    PowerGraph og = new PowerGraph();
+                    //reflow from other end
+                    og.reflow(other);
+                }
+            }else if(linkValid(entity, other) && valid && power.links.size < maxNodes){
+
+                power.links.addUnique(other.pos());
+
+                if(other.team == entity.team){
+                    other.power.links.addUnique(entity.pos());
+                }
+
+                power.graph.addGraph(other.power.graph);
+            }
+        });
+
+        config(Point2[].class, (tile, value) -> {
+            IntSeq old = new IntSeq(tile.power.links);
+
+            //clear old
+            for(int i = 0; i < old.size; i++){
+                configurations.get(Integer.class).get(tile, old.get(i));
+            }
+
+            //set new
+            for(Point2 p : value){
+                configurations.get(Integer.class).get(tile, Point2.pack(p.x + tile.tileX(), p.y + tile.tileY()));
+            }
+        });
+    }
+
+    @Override
+    public void init() {
+        super.init();
+        consumePowerBuffered(powerStorage);
     }
 
     @Override
     public void setBars(){
         super.setBars();
 
-        addBar("power", (ProtocolCoreBlock.ProtocolCoreBuild entity) -> new Bar(() ->
+        addBar("power", (ProtocolCoreBuild entity) -> new Bar(() ->
                 Core.bundle.format("bar.poweroutput",
                         Strings.fixed(entity.getPowerProduction() * 60 * entity.timeScale(), 1)),
                 () -> Pal.powerBar,
-                () -> entity.productionEfficiency));
+                () -> 1f
+        ));
+        addBar("powerstored", (ProtocolCoreBuild entity) -> new Bar(
+                () ->  Core.bundle.format("bar.powerstored",
+                        Strings.fixed(entity.power.status * powerStorage, 1),
+                        powerStorage),
+                () -> Pal.powerBar,
+                () -> entity.power.status
+        ));
+    }
+
+    protected static boolean overlaps(float srcx, float srcy, Tile other, Block otherBlock, float range){
+        return Intersector.overlaps(Tmp.cr1.set(srcx, srcy, range), Tmp.r1.setCentered(other.worldx() + otherBlock.offset, other.worldy() + otherBlock.offset,
+                otherBlock.size * tilesize, otherBlock.size * tilesize));
+    }
+
+    protected boolean overlaps(float srcx, float srcy, Tile other, float range){
+        return Intersector.overlaps(Tmp.cr1.set(srcx, srcy, range), other.getHitbox(Tmp.r1));
+    }
+
+    protected boolean overlaps(Building src, Building other, float range){
+        return overlaps(src.x, src.y, other.tile, range);
+    }
+
+    protected boolean overlaps(Tile src, Tile other, float range){
+        return overlaps(src.drawx(), src.drawy(), other, range);
+    }
+
+    public boolean overlaps(@Nullable Tile src, @Nullable Tile other){
+        if(src == null || other == null) return true;
+        return Intersector.overlaps(Tmp.cr1.set(src.worldx() + offset, src.worldy() + offset, laserRange * tilesize), Tmp.r1.setSize(size * tilesize).setCenter(other.worldx() + offset, other.worldy() + offset));
+    }
+
+    public boolean linkValid(Building tile, Building link){
+        return linkValid(tile, link, true);
+    }
+
+    public boolean linkValid(Building tile, Building link, boolean checkMaxNodes){
+        if(tile == link || link == null || !link.block.hasPower || !link.block.connectedPower || tile.team != link.team || (sameBlockConnection && tile.block != link.block)) return false;
+
+        if(overlaps(tile, link, laserRange * tilesize) || (link.block instanceof PowerNode node && overlaps(link, tile, node.laserRange * tilesize))){
+            if(checkMaxNodes && link.block instanceof PowerNode node){
+                return link.power.links.size < node.maxNodes || link.power.links.contains(tile.pos());
+            }
+            return true;
+        }
+        return false;
+    }
+
+    protected void setupColor(float satisfaction){
+        Draw.color(Tmp.c1.set(Color.white).lerp(Pal.powerLight, (1f - satisfaction) * 0.86f + Mathf.absin(3f, 0.1f)).a(Renderer.laserOpacity));
+    }
+
+    public void drawLaser(float x1, float y1, float x2, float y2, int size1, int size2) {
+        Drawf.laser(laser, laserEnd, x1, y1, x2, y2, 0.25f);
+    }
+
+    /** Iterates through linked nodes of a block at a tile. All returned buildings are power nodes. */
+    public static void getNodeLinks(Tile tile, Block block, Team team, Cons<Building> others){
+        Boolf<Building> valid = other -> other != null && other.tile != tile && other.block instanceof PowerNode node &&
+                node.autolink &&
+                other.power.links.size < node.maxNodes &&
+                overlaps(other.x, other.y, tile, block, node.laserRange * tilesize) && other.team == team
+                && !graphs.contains(other.power.graph) &&
+                !PowerNode.insulated(tile, other.tile) &&
+                !Structs.contains(Edges.getEdges(block.size), p -> { //do not link to adjacent buildings
+                    var t = world.tile(tile.x + p.x, tile.y + p.y);
+                    return t != null && t.build == other;
+                });
+
+        tempBuilds.clear();
+        graphs.clear();
+
+        //add conducting graphs to prevent double link
+        for(var p : Edges.getEdges(block.size)){
+            Tile other = tile.nearby(p);
+            if(other != null && other.team() == team && other.build != null && other.build.power != null
+                    && !(block.consumesPower && other.block().consumesPower && !block.outputsPower && !other.block().outputsPower)){
+                graphs.add(other.build.power.graph);
+            }
+        }
+
+        if(tile.build != null && tile.build.power != null){
+            graphs.add(tile.build.power.graph);
+        }
+
+        var rangeWorld = laserRange * tilesize;
+        var tree = team.data().buildingTree;
+        if(tree != null){
+            tree.intersect(tile.worldx() - rangeWorld, tile.worldy() - rangeWorld, rangeWorld * 2, rangeWorld * 2, build -> {
+                if(valid.get(build) && !tempBuilds.contains(build)){
+                    tempBuilds.add(build);
+                }
+            });
+        }
+
+        tempBuilds.sort((a, b) -> {
+            int type = -Boolean.compare(a.block instanceof PowerNode, b.block instanceof PowerNode);
+            if(type != 0) return type;
+            return Float.compare(a.dst2(tile), b.dst2(tile));
+        });
+
+        tempBuilds.each(valid, t -> {
+            graphs.add(t.power.graph);
+            others.get(t);
+        });
     }
 
     public class ProtocolCoreBuild extends CoreBuild {
         public float generateTime;
-        /** The efficiency of the producer. An efficiency of 1.0 means 100% */
-        public float productionEfficiency = 0.0f;
 
         @Override
         public float getPowerProduction(){
-            return enabled ? powerProduction * productionEfficiency : 0f;
+            return enabled ? powerProduction: 0f;
         }
 
         @Override
         public float warmup(){
-            return enabled ? productionEfficiency : 0f;
+            return enabled ? 1f : 0f;
         }
 
         @Override
@@ -79,17 +269,37 @@ public class ProtocolCoreBlock extends CoreBlock {
         @Override
         public void write(Writes write){
             super.write(write);
-            write.f(productionEfficiency);
             write.f(generateTime);
         }
 
         @Override
         public void read(Reads read, byte revision){
             super.read(read, revision);
-            productionEfficiency = read.f();
             if(revision >= 1){
                 generateTime = read.f();
             }
+        }
+
+        @Override
+        public void draw(){
+            super.draw();
+
+            if(Mathf.zero(Renderer.laserOpacity) || isPayload() || team == Team.derelict) return;
+
+            Draw.z(Layer.power);
+            setupColor(power.graph.getSatisfaction());
+
+            for(int i = 0; i < power.links.size; i++){
+                Building link = world.build(power.links.get(i));
+
+                if(!linkValid(this, link)) continue;
+
+                if(link.block instanceof PowerNode && link.id >= id) continue;
+
+                drawLaser(x, y, link.x, link.y, size, link.block.size);
+            }
+
+            Draw.reset();
         }
     }
 }
